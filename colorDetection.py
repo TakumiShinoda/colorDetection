c7d34b01531219840540690181e4d4563b7cfec7
Mac User <macuser@Mac-no-MacBook-Air-4.local>
2017-01-24 11:59:10 +0900

    first commit

--- colorDetection.py
+++ colorDetection.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-import os
 
 cap = cv2.VideoCapture(0)
 frame_degree = 1
@@ -11,16 +10,12 @@ upper = np.array([25,173,229])
 def display(frame):
     cv2.imshow("frame",frame)
 
-while True:
+def detection(frame):
     count = 0
-    ret,frame = cap.read()
     size = (frame.shape[1] / frame_degree,frame.shape[0] / frame_degree)
-
     frame = cv2.resize(frame,size)
-    h = frame.shape[0]
-    w = frame.shape[1]
-    pixels = h * w
-
+    pixels = frame.shape[0] * frame.shape[1]
+    
     frameSmooth = cv2.medianBlur(frame,7);
     frameHSV = cv2.cvtColor(frameSmooth,cv2.cv.CV_BGR2HSV)
     
@@ -30,9 +25,28 @@ while True:
     for content in frameHSV:
         count += np.count_nonzero(content)
 
-    if count > min_pixels:
-        print("detected")
+    if count > min_pixels: 
+        return count
+    else:
+        return 0
+
+while True:
+    left = 0
+    right = 0
+    ret,frame = cap.read()
+    h = frame.shape[0]
+    w = frame.shape[1]
+    frame_left = frame[0:h,w / 2:w]
+    frame_right = frame[0:h,0:w / 2]
     
-    display(frameHSV)
+    left = detection(frame_left)
+    right = detection(frame_right)
+    
+    if left > right:
+        print("left")
+    elif left < right:
+        print("right")
+
+    display(cv2.inRange(cv2.cvtColor(cv2.medianBlur(frame,7),cv2.cv.CV_BGR2HSV),lower,upper))
             
     cv2.waitKey(1)
